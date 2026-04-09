5d69c5c33cc6c854446f54d5448d2f4e470bf49f
David Jaroš - DJ <david.jaros@chyron.com>
2026-04-09 11:08:41 +0200

    Scalar API added

--- SmartTasks/Program.cs
+++ SmartTasks/Program.cs
@@ -1,12 +1,30 @@
 using StackExchange.Redis;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 
 // 1. Connection to Redis (name 'cache' is in my docker-compose.yml)
 var redis = ConnectionMultiplexer.Connect("cache:6379");
 builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    // generate API file
+    app.MapOpenApi();
+
+    // activate Scalar API reference
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("SmartTasks API")
+               .WithTheme(ScalarTheme.Moon)
+               .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+}
 
 // 2. Save by IConnectionMultiplexer
 app.MapGet("/save/{task}", async (string task, IConnectionMultiplexer redisControl) =>
@@ -23,5 +41,25 @@ app.MapGet("/list/{task}", async (string task, IConnectionMultiplexer redisContr
     string? value = await db.StringGetAsync(task); // read all even from console
     return value ?? "Value was not found";
 });
+
+// 4. The root address returns a list of DB keys
+app.MapGet("/", async (IConnectionMultiplexer redisControl) => {
+    var db = redisControl.GetDatabase();
+var server = redisControl.GetServer("cache", 6379);
+
+var keys = server.Keys();
+var results = new Dictionary<string, string>();
+
+foreach (var key in keys)
+{
+    var value = await db.StringGetAsync(key);
+    results.Add(key.ToString(), value.ToString());
+}
+
+return results.Any() ? Results.Ok(results) : Results.Content("Redis is empty.");
+});
+
+// 5. gets current environment name
+app.MapGet("/env", (IWebHostEnvironment env) => env.EnvironmentName);
 
 app.Run();
